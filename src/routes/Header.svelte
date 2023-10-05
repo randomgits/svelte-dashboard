1786b89d55ff94ef4df89ee97c7af4647d0b59f7
randomgits <123908238+randomgits@users.noreply.github.com>
2023-10-05 13:17:50 +0200

    Create Header.svelte

---
+++ src/routes/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+  // You can add logic for the header here, e.g., navigation
+</script>
+
+<header>
+  <!-- Put your header content here -->
+  <nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+  </nav>
+</header>
