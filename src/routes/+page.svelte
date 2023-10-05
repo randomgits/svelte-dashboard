317957a367e47f1f3ec3644f5fb72be063bfe19b
randomgits <123908238+randomgits@users.noreply.github.com>
2023-10-05 13:22:08 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,8 @@
-<script lang="ts">
-	let userId: string = '';
-	let password: string = '';
-	let isAuthenticated: boolean = false;
-	let taskInput: string = '';
-	let tasks: any[] = [];
-	let credits: number = 4; 
+<script>
+	import { goto } from '$app/navigation';
 
-	const DEMO_USER_ID: string = 'demoUser';
-	const DEMO_PASSWORD: string = 'demoPass';
-
-	async function login(): Promise<void> {
-		if(userId === DEMO_USER_ID && password === DEMO_PASSWORD) {
-			isAuthenticated = true;
-			// Fetch initial data if needed here...
-		} else {
-			alert('Invalid credentials!');
-		}
-	}
-
-	async function addTask(): Promise<void> {
-		// ... Add task logic from the previous example
-	}
+	// Redirect to login page (or you can simply display a 404 message)
+	goto('/login');
 </script>
 
-{#if !isAuthenticated}
-	<!-- Login Page -->
-	<!-- ... as before -->
-{:else}
-	<!-- Dashboard -->
-	<!-- ... as before but with {credits} instead of {$credits} -->
-{/if}
+<p>404 - Page Not Found</p>
