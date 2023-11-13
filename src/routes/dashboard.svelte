3afcb9aa813ffaaf88ff529db220093079e99127
Random <123908238+randomgits@users.noreply.github.com>
2023-11-13 21:39:14 +0000

    Working dashboard.svelte

--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -3,12 +3,10 @@
 	let tasks: any[] = [];
 	let credits: number = 4;
 
-
-
 	async function addTask(): Promise<void> {
 		event.preventDefault();
 		console.log("addTask called");
-		if(taskInput.trim() !== '' && credits > 0) {
+		if (taskInput.trim() !== '' && credits > 0) {
 			const taskId = tasks.length;
 			tasks = [...tasks, {id: taskId, name: `Task ${taskId + 1}`, status: 'Queued', result: null}];
 
@@ -20,7 +18,7 @@
 
 			const data = await response.json();
 			console.log("Response data:", data); 
-			if(response.ok) {
+			if (response.ok) {
 				if (data.task_id) {
 					pollForTaskCompletion(data.task_id.toString(), taskId);
 					console.log("Task ID:", data.task_id);
@@ -35,45 +33,39 @@
 		}
 	}
 
+    async function pollForTaskCompletion(taskId, localTaskId) {
+        const interval = setInterval(async () => {
+            try {
+                //const response = await fetch(`/api/task_status/${taskId}`);
+                const response = await fetch(`https://cloud-run-microservice-template-python-razcvdqurq-uc.a.run.app/task_status/${taskId}`);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
 
-	async function pollForTaskCompletion(taskId, localTaskId) {
-		console.log(`Starting to poll for task ID: ${taskId}`);
-		const interval = setInterval(async () => {
-			console.log(`Polling for task status. Task ID: ${taskId}`);
-			try {
-				console.log(`Fetching task status for ID: ${taskId}`);
-				//const response = await fetch(`/api/task_status/${taskId}`);
-				const response = await fetch(`https://cloud-run-microservice-template-python-razcvdqurq-uc.a.run.app/task_status/${taskId}`);
-				console.log(`Fetched task status for ID: ${taskId}`);
-				if (!response.ok) {
-					throw new Error(`HTTP error! status: ${response.status}`);
-				}
-				const data = await response.json();
-				console.log(`Received data for task ID: ${taskId}`, data);
-				if(data.status === 'Complete') {
-					clearInterval(interval);
-					updateTaskStatus(localTaskId, data.result);
-				}
-			} catch (error) {
-				console.error("Error fetching task status:", error.message);
-				clearInterval(interval); // Consider removing this line if you want to continue polling despite errors
-			}
-		}, 5000); // Poll every 5 seconds
-	}
+                if (data.status === 'Complete') {
+                    clearInterval(interval);
+                    updateTaskStatus(localTaskId, 'Complete', data.result);
+                } else if (data.status === 'In progress') {
+                    updateTaskStatus(localTaskId, 'Processing', null);
+                }
+            } catch (error) {
+                console.error("Error fetching task status:", error.message);
+                clearInterval(interval); // Stop polling on error
+            }
+        }, 5000); // Poll every 5 seconds
+    }
 
-
-
-    function updateTaskStatus(localTaskId, result) {
+    function updateTaskStatus(localTaskId, status, result) {
         tasks = tasks.map(task => {
             if (task.id === localTaskId) {
-                return { ...task, status: 'Complete', result: result };
+                return { ...task, status: status, result: result };
             }
             return task;
         });
     }
 
 	function toggleAccordion(taskId) {
-        // Find the task in the tasks array
         tasks = tasks.map(task => {
             if (task.id === taskId) {
                 return { ...task, showResult: !task.showResult }; // Toggle the showResult property
@@ -81,7 +73,6 @@
             return task;
         });
 	}
-
 </script>
 
 <h1>Dashboard</h1>
@@ -94,14 +85,37 @@
 {#each tasks as task}
     <div>
         <p>{task.name} {task.status}</p>
+        {#if task.status === 'Queued' || task.status === 'Processing'}
+            <div class="loading-bar">
+                <div class="loading-indicator"></div>
+            </div>
+        {/if}
         {#if task.status === 'Complete'}
             <button on:click={() => toggleAccordion(task.id)}>See Result</button>
             <div class="accordion-content" style="{task.showResult ? 'display: block;' : 'display: none;'}">
                 <p>Reversed Text: {task.result}</p>
             </div>
         {/if}
-        {#if task.status === 'Queued'}
-            <!-- Display progress bar here -->
-        {/if}
     </div>
 {/each}
+
+<style>
+    .loading-bar {
+        width: 100%;
+        background-color: #e0e0e0;
+        border-radius: 2px;
+    }
+
+    .loading-indicator {
+        height: 4px;
+        width: 100%;
+        background-color: #3b76f6;
+        border-radius: 2px;
+        animation: loading-animation 2s infinite;
+    }
+
+    @keyframes loading-animation {
+        0% { transform: translateX(-100%); }
+        100% { transform: translateX(100%); }
+    }
+</style>
