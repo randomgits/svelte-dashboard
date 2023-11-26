08dfede78a869563e9dd64cdf8bdea48f21cfc9e
Random <123908238+randomgits@users.noreply.github.com>
2023-11-26 10:20:54 +0000

    style: Update Header and UserProfile components
    Update import paths, add user dropdown menu, and display user email in UserProfile component.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,20 +1,46 @@
-<!-- src/components/Header.svelte -->
 <script>
-  import { auth } from '../firebase.js';
+  import { user } from '$lib/auth.js';
+  import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
+
+  let showDropdown = false;
+  let currentUser = null;
+
+  onMount(() => {
+    $: currentUser = $user ? $user.email : null; // Assuming email as username
+  });
+
+  function toggleDropdown() {
+    showDropdown = !showDropdown;
+  }
 
   async function logout() {
     await auth.signOut();
-    location.href = '/login';
+    goto('/login');
   }
 </script>
 
 <header class="bg-primary text-white shadow-md">
   <nav class="container mx-auto px-6 py-3 flex justify-between items-center">
-  <a class="text-xl font-semibold" href="/dashboard">Your Brand</a>
+    <a class="text-xl font-semibold" href="/dashboard">Your Brand</a>
     <div class="flex items-center">
-      <a class="text-lg mx-2 hover:text-secondary" href="/login">Login</a>
+      {#if currentUser}
+        <div class="relative">
+          <button class="text-lg mx-2 hover:text-secondary" on:click={toggleDropdown}>Profile</button>
+          {#if showDropdown}
+            <div class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
+              <p class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Welcome {currentUser}!</p>
+              <a href="/user-profile" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profile</a>
+              <a href="/credits" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Credits</a>
+              <a href="/settings" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
+              <a href="/login" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" on:click={logout}>Logout</a>
+            </div>
+          {/if}
+        </div>
+      {:else}
+        <a class="text-lg mx-2 hover:text-secondary" href="/login">Login</a>
+      {/if}
       <a class="text-lg mx-2 hover:text-secondary" href="/dashboard">Dashboard</a>
-      <button class="bg-secondary text-white px-4 py-2 rounded hover:bg-secondary-dark" on:click={logout}>Logout</button>
     </div>
   </nav>
 </header>
---
+++ src/components/UserProfile.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { user } from '$lib/auth.js';
+
+  let currentUser;
+  $: if (user) {
+    currentUser = $user.email; // assuming the user object has an email field
+  }
+</script>
+
+<div class="p-4 bg-white shadow rounded">
+  <h1 class="text-3xl font-bold">User Profile</h1>
+  <p class="text-xl mb-4">Email: {currentUser}</p>
+  <!-- Add more user details here -->
+</div>
